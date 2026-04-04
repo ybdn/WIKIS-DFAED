9ed13b0e465eecdac01b65d0e23a7be7a6a11357
Yoann Baudrin <baudrin.yoann@gmail.com>
2026-04-04 20:40:22 +0200

    refactor: update mission and absence color palettes and standardize labels in planning configurations

--- wiki-DocDFAED/staging_area/dsfr/planning/MissionsJournalier.js
+++ wiki-DocDFAED/staging_area/dsfr/planning/MissionsJournalier.js
@@ -10,24 +10,29 @@
  *   fg    : Couleur du texte (hexadecimal)
  */
 window.PlanningMissionsJournalier = [
-    { code: "",    label: "(vide)",                    bg: "#FFFFFF", fg: "#3a3a3a" },
+    { code: "",      label: "(vide)",                        bg: "#FFFFFF", fg: "#3a3a3a" },
 
-    { code: "MMO", label: "MetaMorpho",                bg: "#E8EDFF", fg: "#000091" },
-    { code: "T41", label: "Alphanu",                   bg: "#FFE8D4", fg: "#716043" },
-    { code: "ID",  label: "Identification",            bg: "#FFD4D8", fg: "#7c0300" },
-    { code: "AD",  label: "Administration de base",    bg: "#fff9d4", fg: "#7c5b00" },
-    { code: "J",   label: "Mission particulière",      bg: "#FFFFFF", fg: "#000000" },
+/* MISSIONS */
+    { code: "MMO",   label: "MetaMorpho",                    bg: "#e8edff", fg: "#000091" },
+    { code: "T41",   label: "Alphanu",                       bg: "#d8f5f5", fg: "#009099" },
+    { code: "ID",    label: "Identification",                bg: "#ffe9e9", fg: "#c9191e" },
+    { code: "AD",    label: "Administration de base",        bg: "#fff3bf", fg: "#695240" },
+    { code: "CA",    label: "Conduite admin. de l'unite",    bg: "#e9edf5", fg: "#2f4077" },
+    { code: "CO",    label: "Correction traces",             bg: "#f5ede8", fg: "#6e3b2a" },
+    { code: "J",     label: "Mission particuliere",          bg: "#f6f6f6", fg: "#3a3a3a" },
 
-    { code: "PO",  label: "Permanence Opérationnelle", bg: "#dad4ff", fg: "#3000b6" },
+/* PERMANENCES */
+    { code: "PO",    label: "Permanence Operationnelle",     bg: "#f2e6fd", fg: "#6a00f4" },
 
-    { code: "F",  label: "Formation",                  bg: "#d4ffda", fg: "#297254" },
+/* FORMATION */
+    { code: "F",     label: "Formation",                     bg: "#dffee6", fg: "#18753c" },
+    { code: "SPORT", label: "Sport",                         bg: "#fee9e2", fg: "#b34000" },
 
+/* MALADIE */
+    { code: "CM",    label: "Conge maladie",                 bg: "#fce8f5", fg: "#7c0059" },
 
-    { code: "SPORT",   label: "Sport",                     bg: "#ffe7d4", fg: "#ce4100" },
-
-
-    { code: "R",   label: "Repos",                     bg: "#a1a1a1", fg: "#424242" },
-    { code: "RR",  label: "Repos récupérateur",        bg: "#a1a1a1", fg: "#424242" },
-    { code: "P",   label: "Permissions",               bg: "#a1a1a1", fg: "#424242" },
-
+/* ABSENCES */
+    { code: "R",     label: "Repos",                         bg: "#eeeeee", fg: "#666666" },
+    { code: "RR",    label: "Repos recuperateur",            bg: "#eeeeee", fg: "#666666" },
+    { code: "P",     label: "Permissions",                   bg: "#eeeeee", fg: "#666666" }
 ];
--- wiki-DocDFAED/staging_area/dsfr/planning/MissionsP4S.js
+++ wiki-DocDFAED/staging_area/dsfr/planning/MissionsP4S.js
@@ -10,20 +10,25 @@
  *   fg    : Couleur du texte (hexadecimal)
  */
 window.PlanningMissionsP4S = [
-    { code: "",   label: "(vide)",                  bg: "#FFFFFF", fg: "#3a3a3a" },
-/* PERMANENCES */    
-    { code: "M",  label: "Matin",                   bg: "#E8EDFF", fg: "#000091" },
-    { code: "AM", label: "Apres-midi",              bg: "#FFE8D4", fg: "#716043" },
-    { code: "WE", label: "Week-end",                bg: "#FFD4D8", fg: "#910000" },
+    { code: "",    label: "(vide)",                  bg: "#FFFFFF", fg: "#3a3a3a" },
+
+/* PERMANENCES */
+    { code: "M",   label: "Matin",                   bg: "#e8edff", fg: "#000091" },
+    { code: "AM",  label: "Apres-midi",              bg: "#fff3bf", fg: "#695240" },
+    { code: "WE",  label: "Week-end",                bg: "#ffe9e9", fg: "#c9191e" },
+
 /* ABSENCES */
-    { code: "R",  label: "Repos",                   bg: "#B8FEC9", fg: "#18753C" },
-    { code: "RR", label: "Repos retard",            bg: "#B8FEC9", fg: "#18753C" },
-    { code: "P",  label: "Permissions",             bg: "#B8FEC9", fg: "#18753C" },
-    { code: "PM",  label: "Permission matin",       bg: "#B8FEC9", fg: "#18753C" },
-    { code: "PAM",  label: "Permission après-midi", bg: "#B8FEC9", fg: "#18753C" },
-    { code: "CM",  label: "Congé maladie",          bg: "#B8FEC9", fg: "#18753C" },
+    { code: "R",   label: "Repos",                   bg: "#dffee6", fg: "#18753c" },
+    { code: "RR",  label: "Repos retard",            bg: "#dffee6", fg: "#18753c" },
+    { code: "P",   label: "Permissions",             bg: "#dffee6", fg: "#18753c" },
+    { code: "PM",  label: "Permission matin",        bg: "#dffee6", fg: "#18753c" },
+    { code: "PAM", label: "Permission apres-midi",   bg: "#dffee6", fg: "#18753c" },
+
+/* MALADIE */
+    { code: "CM",  label: "Conge maladie",           bg: "#fce8f5", fg: "#7c0059" },
+
 /* FORMATION */
-    { code: "F",  label: "Formation",               bg: "#B8FEC9", fg: "#18753C" },
-    { code: "TIR", label: "Tir",                    bg: "#B8FEC9", fg: "#18753C" },
-    { code: "OPJ", label: "OPJ",                    bg: "#B8FEC9", fg: "#18753C" }
+    { code: "F",   label: "Formation",               bg: "#f2e6fd", fg: "#6a00f4" },
+    { code: "TIR", label: "Tir",                     bg: "#f2e6fd", fg: "#6a00f4" },
+    { code: "OPJ", label: "OPJ",                     bg: "#f2e6fd", fg: "#6a00f4" }
 ];
